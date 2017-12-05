df3c2537db90c753b65b221550006481a9c40cda
chrisni123 <chrisni@udel.edu>
2017-12-05 10:38:14 -0500

    added movies

--- manage.py
+++ manage.py
@@ -9,7 +9,17 @@ def deploy():
     db.drop_all()
     db.create_all()
     x = X()
-    db.session.add()
+    movie1 = Movie(title= 'The Lion King', description='Plot: tells the story of Simba, a young lion who is to succeed his father, Mufasa, as King of the Pride Lands; however, after Simbas uncle Scar, murders Mufasa, Simba is manipulated into thinking he was responsible and flees into exile. Upon maturation living with two wastrels, Simba is given some valuable perspective from his childhood friend, Nala, and his shaman, Rafiki, before returning to challenge Scar to end his tyranny and take his place in the Circle of Life as the rightful King.')
+    movie2 = Movie(title='Mulan', description='Plot: Fearful that her ailing father will be drafted into the Chinese military, Mulan (Ming-Na Wen) takes his spot -- though, as a girl living under a patriarchal regime, she is technically unqualified to serve.')
+    movie3 = Movie(title='Moana', description='An adventurous teenager sails out on a daring mission to save her people.')
+    movie4 = Movie(title='Frozen', description='When their kingdom becomes trapped in perpetual winter, fearless Anna (Kristen Bell) joins forces with mountaineer Kristoff (Jonathan Groff) and his reindeer sidekick to find Annas sister, Snow Queen Elsa (Idina Menzel), and break her icy spell.')
+    movie5 = Movie(title='Cinderella', description='With a wicked stepmother (Eleanor Audley) and two jealous stepsisters (Rhoda Williams, Lucille Bliss) who keep her enslaved and in rags, Cinderella (Ilene Woods) stands no chance of attending the royal ball.')
+
+    db.session.add(movie1)
+    db.session.add(movie2)
+    db.session.add(movie3)
+    db.session.add(movie4)
+    db.session.add(movie5)
     db.session.commit()
 
 
--- project.py
+++ project.py
@@ -11,11 +11,26 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 db = SQLAlchemy(app)
 
+class Movie(db.Model):
+    __tablename__ = 'movies'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(256))
+    description = db.Column(db.Text)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/movies')
+def get_all_movies():
+    movies =[
+    'The Lion King',
+    'Mulan',
+    'Moana',
+    'Frozen',
+    'Cinderella'
+    ]
+    return render_template('movies.html', movies = movies)
 
 @app.route('/members')
 def members_page():
